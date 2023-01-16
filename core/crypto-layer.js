30f2c6d33aee0ae0f66e85f4a99a3fec47ff35bf
AchintyaSai <amazingachintya@gmail.com>
2023-01-16 17:15:04 +0530

    Encryption Key created in environment

--- core/crypto-layer.js
+++ core/crypto-layer.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const decryptBody = (req,res,next) => {
     if(process.env.ENCRYPTION_ENABLED == "true"){
-        var decryptedBody  = crypto.AES.decrypt(req.body.encryptedBody, '999999999').toString(crypto.enc.Utf8);
+        var decryptedBody  = crypto.AES.decrypt(req.body.encryptedBody, process.env.ENCRYPTION_KEY).toString(crypto.enc.Utf8);
         req.body = JSON.parse(decryptedBody);
     }
     console.log(req.body)
@@ -13,7 +13,7 @@ const decryptBody = (req,res,next) => {
 const encryptResponse = (data, res) => {
     let stringifiedRes = JSON.stringify(data);
     if(process.env.ENCRYPTION_ENABLED == "true"){
-        var encryptRes = crypto.AES.encrypt(stringifiedRes, '999999999').toString();
+        var encryptRes = crypto.AES.encrypt(stringifiedRes, process.env.ENCRYPTION_KEY).toString();
         res.json({ 'encryptedResponse' : encryptRes});
     }
     else{
